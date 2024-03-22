13324059672d2bc1c276595c100781c975c39bc0
Denis Derkach <derkach.denis@lll.kpi.ua>
2024-03-22 17:01:48 +0200

    Added logs to the service layer

--- src/main/java/com/example/flatservice/service/impl/FlatServiceImpl.java
+++ src/main/java/com/example/flatservice/service/impl/FlatServiceImpl.java
@@ -14,6 +14,7 @@ import com.example.flatservice.service.FlatService;
 import com.example.flatservice.specification.FlatSpecification;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -24,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class FlatServiceImpl implements FlatService {
@@ -33,43 +35,60 @@ public class FlatServiceImpl implements FlatService {
     private final FlatMapperForInformationPage flatMapperForInformationPage;
     @Override
     public Page<FlatDtoForViewAll> getAllForAdminByCorpId(Integer page, Integer pageSize, Long corpId) {
+        log.info("FlatServiceImpl-getAllForAdminByCorpId start");
         Pageable pageable = PageRequest.of(page, pageSize, Sort.by(Sort.Order.desc("id")));
-        return flatMapperForViewAll.toDtoPage(flatRepository.findAllByCorps(corpId, pageable));
+        Page<FlatDtoForViewAll> res = flatMapperForViewAll.toDtoPage(flatRepository.findAllByCorps(corpId, pageable));
+        log.info("FlatServiceImpl-getAllForAdminByCorpId finish");
+        return res;
     }
 
     @Override
     public Page<FlatDtoForViewAll> getAllForCustomer(FlatDtoForFilter flatDtoForFilter) {
+        log.info("FlatServiceImpl-getAllForCustomer start");
         Specification<Flat> specification = new FlatSpecification(flatDtoForFilter);
         Pageable pageable = PageRequest.of(flatDtoForFilter.getPage(), flatDtoForFilter.getPageSize(), Sort.by(Sort.Order.desc("id")));
         Page<FlatDtoForViewAll> result =  flatMapperForViewAll.toDtoPage(flatRepository.findAll(specification, pageable));
+        log.info("FlatServiceImpl-getAllForCustomer finish");
         return result;
     }
     @Override
     public Flat getById(Long flatId) {
-        return flatRepository.findById(flatId).orElseThrow(
+        log.info("FlatServiceImpl-getById start");
+        Flat flat = flatRepository.findById(flatId).orElseThrow(
                 ()-> {
                     throw new EntityNotFoundException("Building with id="+flatId+" not found");
                 }
         );
+        log.info("FlatServiceImpl-getById finish");
+        return flat;
     }
     @Override
     public void changeStatus(Long flatId, StatusState statusState) {
+        log.info("FlatServiceImpl-changeStatus start");
         Flat flat = getById(flatId);
         flat.setStatusState(statusState);
         save(flat);
+        log.info("FlatServiceImpl-changeStatus finish");
     }
     @Override
     @Transactional
     public void save(Flat flat) {
+        log.info("FlatServiceImpl-save start");
         flatRepository.save(flat);
+        log.info("FlatServiceImpl-save finish");
     }
     @Override
     public FlatDtoForInformationPage getFlatForInformationPage(Long flatId) {
-        return flatMapperForInformationPage.toDtoFromEntity(getById(flatId));
+        log.info("FlatServiceImpl-getFlatForInformationPage start");
+        FlatDtoForInformationPage flatDtoForInformationPage = flatMapperForInformationPage.toDtoFromEntity(getById(flatId));
+        log.info("FlatServiceImpl-getFlatForInformationPage finish");
+        return flatDtoForInformationPage;
     }
     @Override
     @Transactional
     public void add(FlatDtoForAdd flatDtoForAdd) throws IOException {
+        log.info("FlatServiceImpl-add start");
         save(flatMapperForAdd.updateEntityFromDto(flatDtoForAdd, this));
+        log.info("FlatServiceImpl-add finish");
     }
 }
